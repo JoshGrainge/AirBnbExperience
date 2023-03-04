226917aea2d61903fa04156ea48a73ddbc6701d7
Josh Grainge <josh.grainge@hotmail.com>
2023-03-04 14:27:28 -0500

    Added nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../air-bnb-logo192.png';
+
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <img className="nav-logo" src={logo} alt="logo" />
+    </nav>
+  );
+}
