6c51bf80ff18522b35ca17fe88fbca382b5a6fbd
Josh Grainge <josh.grainge@hotmail.com>
2023-03-04 18:15:16 -0500

    Created card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import katieThumb from './Katie-Zaferes.jpeg';
 import './App.css';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
@@ -9,7 +10,25 @@ function App() {
     <div>
       <Nav />
       <Hero />
-      <Card />
+      <Card
+        image={katieThumb}
+        starAvg="5.0"
+        reviewNum={6}
+        country="USA"
+        description="Life lessons with Katie Zaferes"
+        price={136}
+        overlayText="SOLD OUT"
+      />
+
+      <Card
+        image={katieThumb}
+        starAvg="4.7"
+        reviewNum="25"
+        country="Canada"
+        description="Another description"
+        price="77"
+        overlayText="ONLINE"
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,33 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import star from '../star.png';
-import thumb from '../Katie-Zaferes.jpeg';
 
-export default function Card() {
+export default function Card({
+  image,
+  starAvg,
+  reviewNum,
+  country,
+  description,
+  price,
+  overlayText,
+}) {
   return (
     <div className="card-container">
-      <img className="thumb" src={thumb} alt="thumbanil" />
+      <img className="thumb" src={image} alt="thumbanil" />
       <div className="star-container">
         <img className="star-icon" src={star} alt="star_icon" />
-        <p>5.0</p>
-        <p>(6)</p>
+        <p>{starAvg}</p>
+        <p>({reviewNum})</p>
         <p>·</p>
-        <p>USA</p>
+        <p>{country}</p>
       </div>
-      <p className="card-description">Life lessons with Katie Zaferes</p>
+      <p className="card-description">{description}</p>
       <div className="pricing">
-        <p>From $136</p>
+        <p>From ${price}</p>
         <p> / person</p>
       </div>
       <div className="card-overlay">
-        <p>SOLD OUT</p>
+        <p>{overlayText}</p>
       </div>
     </div>
   );
